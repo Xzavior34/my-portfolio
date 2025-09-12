0ebf9db1646c10fc79b7d6a43ad0628d07570348
Xzavior34 <philipinem7@gmail.com>
2025-09-12 16:09:02 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './index.css';
+import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
