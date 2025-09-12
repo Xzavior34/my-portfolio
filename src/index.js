db150f16d917f3ec97285b88f4192eb921400766
Xzavior34 <philipinem7@gmail.com>
2025-09-12 21:54:34 +0100

    Simplify portfolio: remove React Router and fix homepage issues

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';           // Your global styles (Tailwind included)
-import App from './App';        // Main App component
+import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-// Optional: use HashRouter if deploying on GitHub Pages
-import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>
 );
 
-// Optional: measure performance
 reportWebVitals();
