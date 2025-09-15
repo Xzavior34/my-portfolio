68edf3cdbb0de693b3ba1d147ae26d3daec2b711
Xzavior34 <philipinem7@gmail.com>
2025-09-15 15:32:40 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 // src/App.js
 import React, { useState } from "react";
-import About from "./components/About";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import About from "./components/About";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
